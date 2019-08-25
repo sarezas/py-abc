745ec47a399b6afd3c5e5a2f56cff24170d994c9
Sharis <sarezas@gmail.com>
2019-08-25 16:58:36 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,123 @@
+# print("Hello, World!")
+
+# first = 9
+# second = first + 1
+
+
+# def add(a, b):
+#     print(a + b)
+
+
+# add(first, second)
+
+# """asdsdsd"""
+
+# cars = ["audi", "mercedes-benz", "bmw", "volkswagen", "volvo", "seat"]
+# name = "Sharis"
+# dumb = False
+
+# if name:
+#     print(name)
+
+
+# if name == "Sharis" and not dumb:
+#     cars.append("opel")
+#     print(cars[:1])
+
+
+# a = 9
+# b = 10
+
+# "bigger" if b > a else "smaller"
+
+# for car in cars:
+#     print("The cars in the list include {0}".format(car))
+
+
+# x = 0
+# for index in range(10):
+#     x += 10
+#     print("The value of x is {0}".format(x))
+
+
+# students = ["Antanas", "Petras", "Stasys", "Mindaugas", "Elze",
+#             "Jurgita", "Sarunas", "Ilona", "Virginija", "Nijole", "Egle", "Pranas"]
+
+# for student in students:
+#     if student == "Sarunas":
+#         print("Found him - " + student)
+#         continue
+#     print("Searching for the match: " + student + " going to next")
+
+
+# phrase = "Sharis is cool"
+
+# for char in phrase:
+#     print(char)
+
+
+# student = {
+#     "name": "sharis",
+#     "last_name": "maris",
+#     "id": 19,
+#     "attendance": "excellent"
+# }
+
+
+# try:
+#     x = student["grades"]
+# except KeyError as error:
+#     print("This key is not found!")
+#     print(error)
+
+# print("This code still executes")
+
+friends = []
+
+
+def get_friends_titlecase():
+    friends_titlecase = []
+    for friend in friends:
+        friends_titlecase.append(friend["name"].title())
+
+    return friends_titlecase
+
+
+def print_friends_titlecase():
+    friends_titlecase = get_friends_titlecase()
+    print(friends_titlecase)
+
+
+def add_friend(name, id=19):
+    friend = {"name": name, "id": id}
+    friends.append(friend)
+
+
+def save_file(friend):
+    try:
+        f = open("friends.txt", "a")
+        f.write(friend + "\n")
+        f.close()
+    except Exception:
+        print("Could not save to file")
+
+
+def read_file():
+    try:
+        f = open("friends.txt", "r")
+        for friend in f.readlines():
+            add_friend(friend)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+
+read_file()
+print_friends_titlecase()
+
+friend_name = input("Enter friend name: ")
+friend_id = int(input("Enter friend ID: "))
+
+add_friend(friend_name, friend_id)
+save_file(friend_name)
+read_file()
