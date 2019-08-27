50140d358932907a5f1b0f3e19ab87e99a110324
sharisM <sarezas@gmail.com>
2019-08-27 10:04:21 +0200

    list comprehension ex. updated

--- hello.py
+++ hello.py
@@ -142,5 +142,6 @@ numbers_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 #print a new list of numbers which are less than 5 from the given numbers list in one line
 print([number for number in numbers_list if number < 5])
+#print([[output] [itteration] [condition]])
 
 # => [1, 1, 2, 3]
