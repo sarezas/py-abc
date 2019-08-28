6b7924486eecc649124a46221d2b6cb1730ab55a
sharisM <sarezas@gmail.com>
2019-08-28 15:02:00 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -158,7 +158,7 @@ def show_divisors(number_from_input):
 nr_from_input = int(input("Give me a number: "))
 show_divisors(nr_from_input)
 
-#given two lists print out a new list only of common items in both lists
+#given two lists print out a new list only of common items in both lists without duplicates
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
