a7a6a1c6e6c8d6098d09777709e89c22f87b638b
sharisM <sarezas@gmail.com>
2019-08-28 11:32:33 +0200

    find common items in two lists program added

--- hello.py
+++ hello.py
@@ -157,3 +157,19 @@ def show_divisors(number_from_input):
                 
 nr_from_input = int(input("Give me a number: "))
 show_divisors(nr_from_input)
+
+#given two lists print out a new list only of common items in both lists
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def find_in_lists(list1, list2):
+    common_numbers = []
+    for number in list1:
+        if number in list2:
+            if number not in common_numbers:
+                common_numbers.append(number)
+                print(common_numbers)
+            
+                
+find_in_lists(a, b) 
